13c548eadabac00821a72565f25a40ae64bde4c1
gfulep <gfulep@diligent.com>
2024-12-02 09:16:21 +0100

    Day 2 prep

---
+++ AdventOfCode2024/Day2/PuzzleSolver.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2024.Common;
+using AdventOfCode2024.Day1;
+
+namespace AdventOfCode2024.Day2;
+
+public class PuzzleSolver : PuzzleSolverBase
+{
+    public PuzzleSolver(IInput input) : base(input)
+    {
+    }
+
+    public override string GetFirstSolution()
+    {
+        return string.Empty;
+    }
+
+    public override string GetSecondSolution()
+    {
+        return string.Empty;
+    }
+}

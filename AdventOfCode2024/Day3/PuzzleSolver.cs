a0f83c391274f9fd2bf8739633054f88381eaabd
Gábor Fülep <fulepgabor@outlook.com>
2024-12-03 09:18:26 +0100

    Day 3 solved

---
+++ AdventOfCode2024/Day3/PuzzleSolver.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode2024.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Day3;
+
+public class PuzzleSolver : PuzzleSolverBase
+{
+    public PuzzleSolver(IInput input) : base(input)
+    {
+    }
+
+    public override string GetFirstSolution()
+    {
+        return CalculateProduct(input.GetRawInput()).ToString();
+    }
+    public override string GetSecondSolution()
+    {
+        var rawInput = input.GetRawInput();
+        var splittedStrings = rawInput.Split("do()", StringSplitOptions.RemoveEmptyEntries);
+        int product = 0;
+        foreach (var splittedString in splittedStrings)
+        {
+            product += CalculateProduct(splittedString.Split("don't()", 2).First());
+        }
+
+        return product.ToString();
+    }
+
+    private int CalculateProduct(string input)
+    {
+        string pattern = @"mul\((?'Num1'\d{1,3}),(?'Num2'\d{1,3})\)";
+        Regex regex = new Regex(pattern);
+
+        var matches = regex.Matches(input);
+        var product = 0;
+        foreach (Match match in matches)
+        {
+            product += int.Parse(match.Groups["Num1"].Value) * int.Parse(match.Groups["Num2"].Value);
+        }
+
+        return product;
+    }
+}
---
+++ SolverTests/Day3/SolverTests.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode2024.Day3;
+using FluentAssertions;
+
+namespace SolverTests.Day3;
+public class SolverTests
+{
+    [Fact]
+    public void FirstSolutionShouldGiveCorrectResultOnDemoInput()
+    {
+        //Arrange
+        string input = @"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        string expectedResult = "161";
+        PuzzleSolver puzzleSolver = new PuzzleSolver(new StringInput(input));
+
+        //Act
+        var solution = puzzleSolver.GetFirstSolution();
+
+        //Assert
+        solution.Should().Be(expectedResult);
+    }
+
+    [Fact]
+    public void SecondSolutionShouldGiveCorrectResultOnDemoInput()
+    {
+        //Arrange
+        string input = @"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        string expectedResult = "48";
+        PuzzleSolver puzzleSolver = new PuzzleSolver(new StringInput(input));
+
+        //Act
+        var solution = puzzleSolver.GetSecondSolution();
+
+        //Assert
+        solution.Should().Be(expectedResult);
+    }
+}

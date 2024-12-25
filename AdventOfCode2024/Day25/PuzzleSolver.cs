3dd4dfe9ca86735d36455f72f885d2664d947bd8
Gábor Fülep <fulepgabor@outlook.com>
2024-12-25 09:38:34 +0100

    Day 25 solved

---
+++ AdventOfCode2024/Day25/PuzzleSolver.cs
@@ -0,0 +1,84 @@
+﻿using AdventOfCode2024.Common;
+
+namespace AdventOfCode2024.Day25;
+
+public class PuzzleSolver : PuzzleSolverBase
+{
+    public PuzzleSolver(IInput input) : base(input)
+    {
+    }
+
+    public override string GetFirstSolution()
+    {
+        List<SafeMatrix<char>> locksAndkeys = new();
+        var enumerator = input.GetEnumerator();
+        bool notEndOfFile = enumerator.MoveNext();
+        while (notEndOfFile)
+        {
+            List<List<char>> matrix = new();
+            while (notEndOfFile && enumerator.Current != string.Empty)
+            {
+                matrix.Add(enumerator.Current.ToList());
+                notEndOfFile = enumerator.MoveNext();
+            }
+            locksAndkeys.Add(new SafeMatrix<char>(matrix));
+            if (notEndOfFile)
+            {
+                enumerator.MoveNext();
+            }
+        }
+
+        List<List<int>> locks = new();
+        List<List<int>> keys = new();
+        foreach (var item in locksAndkeys)
+        {
+            List<int> heights = new();
+            foreach (var column in item.GetColumns())
+            {
+                heights.Add(column.Count(ch => ch == '#') - 1);
+            }
+
+            if (item.GetRow(0).All(ch => ch == '#'))
+            {
+                // It's a lock
+                locks.Add(heights);
+            }
+            else
+            {
+                keys.Add(heights);
+            }
+        }
+
+        var matchWidouthOverlapping = 0;
+        foreach (var key in keys)
+        {
+            foreach (var doorLock in locks)
+            {
+                if (IsKeyFitToLock(key, doorLock))
+                {
+                    matchWidouthOverlapping++;
+                }
+            }
+        }
+
+        return matchWidouthOverlapping.ToString();
+    }
+
+    private bool IsKeyFitToLock(List<int> key, List<int> doorLock)
+    {
+        for (int i = 0; i < key.Count; i++)
+        {
+            if (key[i] + doorLock[i] > 5)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override string GetSecondSolution()
+    {
+        return "";
+    }
+}
---
+++ SolverTests/Day25/SolverTests.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCode2024.Day25;
+using FluentAssertions;
+
+namespace SolverTests.Day25;
+
+public class SolverTests
+{
+    private readonly string input = @"#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####";
+
+    [Fact]
+    public void FirstSolutionShouldGiveCorrectResultOnDemoInput()
+    {
+        //Arrange
+        string expectedResult = "3";
+        PuzzleSolver puzzleSolver = new PuzzleSolver(new StringInput(input));
+
+        //Act
+        var solution = puzzleSolver.GetFirstSolution();
+
+        //Assert
+        solution.Should().Be(expectedResult);
+    }
+}
